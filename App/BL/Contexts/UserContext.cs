398454b75d42fab7f9ca8a9054f2a35e4df53f24
Aleksandr <Aleksandr_egorov_07@mail.ru>
2026-01-17 09:06:33 +0400

    Добавил UserRepository

--- App/BL/Contexts/UserContext.cs
+++ App/BL/Contexts/UserContext.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace App.BL.Contexts
+﻿namespace App.BL.Contexts
 {
     class UserContext
     {
---
+++ App/BL/Data/Repositories/UserRepository.cs
@@ -0,0 +1,11 @@
+﻿using App.BL.Data.DbModels;
+
+namespace App.BL.Data.Repositories
+{
+    public class UserRepository : BaseRepository<UserDbModel>
+    {
+        public UserRepository(AppDbContext appDbContext) : base(appDbContext)
+        {
+        }
+    }
+}
